549f42914114f2cb972394b99bbc23277493e1c0
kristians2005 <mikelsons3333@gmail.com>
2024-05-20 13:38:34 +0300

    storage routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::delete('/logout', [AuthController::class, 'destroy'])->name('logout');
 
 
 Route::get('/storage', [StorageController::class, 'index'])->name('storage.index');
+Route::get('/storage/{storage}', [StorageController::class, 'show'])->name('storage.show');
+Route::get('/storage/{storage}/edit', [StorageController::class, 'edit'])->name('storage.edit');
+Route::delete('/storage/{storage}', [StorageController::class, 'edit'])->name('storage.destroy');
+Route::put('/storage/{storage}', [StorageController::class, 'update'])->name('storage.update');
+Route::post('/storage', [StorageController::class, 'store'])->name('storage.store');
+Route::get('/storage/create', [StorageController::class, 'create'])->name('storage.create');
