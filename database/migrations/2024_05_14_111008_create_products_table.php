744193c95da91490e82a1ef764b1a9f907fdedc6
Raivo Kinne <120594466+raivokinne@users.noreply.github.com>
2024-05-20 13:21:33 +0300

    fixing database tables

--- database/migrations/2024_05_14_111008_create_products_table.php
+++ database/migrations/2024_05_14_111008_create_products_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create(
-            'products', function (Blueprint $table) {
+            'products',
+            function (Blueprint $table) {
                 $table->id();
                 $table->string('name');
                 $table->string('description');
                 $table->float('price');
                 $table->foreignId('supplier_id')->constrained('suppliers');
-                $table->foreignId('shelf_id')->constrained('shelves');
                 $table->integer('stock');
-                $table->foreignId('user_id')->constrained('users');
-                $table->foreignId('storage_id')->constrained('storages');
                 $table->boolean('active')->default(true);
                 $table->softDeletes();
                 $table->timestamps();
