529381867f2c252b022062f05da5a7420ef7d0f0
kristians2005 <mikelsons3333@gmail.com>
2024-05-20 13:32:04 +0300

    storage route thigy

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class StorageController extends Controller
 {
     
     public function index() {
 
+        return inertia::render('Storage/Index');
 
-            
     }
 
 
