bab15e52133c1296a227d77f1fffc60432fb1766
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-05-20 14:24:24 +0300

    Changes

--- database/migrations/2024_05_14_111039_create_reports_table.php
+++ database/migrations/2024_05_14_111039_create_reports_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
