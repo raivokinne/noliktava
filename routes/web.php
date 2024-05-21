e74c1f61b204bca15db6e77c791c929f6e84931a
kristians2005 <mikelsons3333@gmail.com>
2024-05-21 12:55:28 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportsController;
 use App\Http\Controllers\StorageController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -13,13 +14,27 @@ Route::get(
     }
 )->name('home');
 
-Route::get('/register', [AuthController::class, 'register'])->name('register')->middleware(IsAdmin::class);
-Route::post('/register', [AuthController::class, 'registerStore'])->name('register.store')->middleware(IsAdmin::class);
+
 Route::get('/login', [AuthController::class, 'create'])->name('login');
 Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('/logout', [AuthController::class, 'destroy'])->name('logout');
 
-Route::get('/users', [ReportsController::class, 'index'])->name('users.index')->middleware(IsAdmin::class);
+
+//routes ko tikai admin var tikt
+Route::middleware(IsAdmin::class)->group(function () {
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerStore'])->name('register.store');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{users}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{users}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::delete('/users/{users}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{users}', [UserController::class, 'update'])->name('users.update');
+    
+});
+
+
 
 Route::get('/storage', [StorageController::class, 'index'])->name('storage.index');
 Route::get('/storage/{storage}', [StorageController::class, 'show'])->name('storage.show');
