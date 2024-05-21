f5659685d03b038b5686fe2940f4cfe738eb6119
kristians2005 <mikelsons3333@gmail.com>
2024-05-21 12:41:58 +0300

    pushing stuff

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReportsController;
 use App\Http\Controllers\StorageController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,12 +13,13 @@ Route::get(
     }
 )->name('home');
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'registerStore'])->name('register.store');
+Route::get('/register', [AuthController::class, 'register'])->name('register')->middleware(IsAdmin::class);
+Route::post('/register', [AuthController::class, 'registerStore'])->name('register.store')->middleware(IsAdmin::class);
 Route::get('/login', [AuthController::class, 'create'])->name('login');
 Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('/logout', [AuthController::class, 'destroy'])->name('logout');
 
+Route::get('/users', [ReportsController::class, 'index'])->name('users.index')->middleware(IsAdmin::class);
 
 Route::get('/storage', [StorageController::class, 'index'])->name('storage.index');
 Route::get('/storage/{storage}', [StorageController::class, 'show'])->name('storage.show');
