23ba57bcd6e8ab210640abaa43ded2c80ec7ecce
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-05-21 13:02:08 +0300

    changes

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -1,10 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class OrderItemController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('OrderItem/Index');
+    }
+
+
+    public function show($id)
+    {
+        $product = OrderItem::find($id);
+        return Inertia::render('OrderItem/Show', compact('orderitem'));
+    }
+
+    public function destroy($id)
+    {
+        $product = OrderItem::find($id);
+        $product->delete();
+        return redirect()->route('OrderItem.index');
+    }
+
+    public function create()
+    {
+        return Inertia::render('OrderItem/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'order_id' =>'required', 
+               'product_id' =>'required',
+               'quantity' =>'required',
+               'price' =>'required|decimal',
+               'total' =>'required'
+            ]
+        );
+
+        $request['user_id'] = auth()->user()->id;
+
+        OrderItem::create($request->all());
+
+        return redirect()->route('OrderItem.index');
+    }
+
 }
