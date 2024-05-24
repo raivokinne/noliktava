1112f58bf035fd10ec2d738dd82af264a36982ab
Raivo Kinne <120594466+raivokinne@users.noreply.github.com>
2024-05-24 11:31:55 +0300

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect()->route('dashboard');
+        return redirect()->route('login');
     }
 
     public function register()
