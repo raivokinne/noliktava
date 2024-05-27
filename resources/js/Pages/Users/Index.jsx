66d33a854d6e672f94adffeec46ba7edc528cc4a
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-05-27 15:16:44 +0300

    search button

--- resources/js/Pages/Users/Index.jsx
+++ resources/js/Pages/Users/Index.jsx
@@ -19,6 +19,8 @@ export default function Index({ users }) {
         return new Date(isoString).toLocaleDateString(undefined, options);
     };
 
+    
+
     return (
         <Authenticated>
             <div className="w-full p-8 bg-white rounded-md">
@@ -29,13 +31,27 @@ export default function Index({ users }) {
                         </h2>
                         <span className="text-xs">All products item</span>
                     </div>
+
                     <div className="flex items-center justify-between">
                         <div className="flex items-center p-2 rounded-md bg-gray-50">
-                            <input
-                                className="w-[400px] border border-black rounded-full p-2 px-4 bg-gray-50"
-                                type="text"
-                                placeholder="search..."
-                            />
+                            <div className='flex items-center justify-center'>
+                                <div className="flex rounded-full border border-gray-300 w-full max-w-[600px] shadow-md">
+                                    <input
+                                        type="text"
+                                        className="w-full bg-white flex bg-transparent pl-2 rounded-l-full text-black outline-0"
+                                        placeholder="Search"
+                                    />
+                                    <button type="submit" className="relative p-2 text-blue-400">
+                                        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
+                                            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <g id="SVGRepo_iconCarrier">
+                                                <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#007bff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+            </div>
+        </div>
                         </div>
                     </div>
                 </div>
