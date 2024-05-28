1ced8f0dce7db97058be6aee28059eef8c0964c3
kristians2005 <mikelsons3333@gmail.com>
2024-05-28 10:22:46 +0300

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Reports;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class UserController extends Controller
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => 'admin',
+            'role' => fake()->randomElement(['admin', 'worker', 'sorter']),
+            'status' => fake()->randomElement(['Active', 'Inactive']),
         ];
     }
 
